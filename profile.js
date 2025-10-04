2cfb81a77b82a3d323a8e4e17ec04120338c9efa
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-10-04 12:02:28 +0300

    create tasks

--- profile.js
+++ profile.js
@@ -20,4 +20,109 @@ logout.addEventListener('click', e => {
   window.location.href = "index.html";
 })
 
+/*Add tasks*/
 
+const form = document.querySelector('.form');
+const todoList = document.querySelector('.todo-items');
+let tasks = [];
+
+loadTasks();
+showTasks();
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const task = Object.fromEntries(new FormData(form));
+  addTask(task);
+  saveTasks();
+  showTasks();
+  form.reset();
+})
+
+function addTask(task) {
+  task.done = false;
+  tasks.push(task);
+}
+
+function showTasks() {
+  let html = '';
+
+  tasks.forEach(task => {
+    html += `
+      <li class="item">
+        <div class="todo-header">
+          <h3>${task.category}</h3>
+          <a href="edit.html" class="btn-edit">
+            <svg widht="40" height="40">
+              <use href="icons/edit.svg" />
+            </svg>
+          </a>
+        </div>
+        <div class="todo-text">
+          <p class="text">${task.title}</p>
+        </div>
+        <div class="control">
+          <div class="check">
+            <label>
+              <input type="checkbox" ${task.done ? "checked" : ""} hidden />
+            </label>
+          </div>
+          <button type="button" class="delete">
+            <svg widht="40" height="40">
+              <use href="icons/delete.svg" />
+            </svg>
+          </button>
+        </div>
+      </li>
+    `;
+  });
+  todoList.innerHTML = html;
+}
+
+function loadTasks() {
+  const json = localStorage.tasks;
+  if (!json) return;
+  const loadedTasks = JSON.parse(json);
+  
+  tasks = loadedTasks
+}
+
+function saveTasks() {
+  const json = JSON.stringify(tasks);
+  
+  localStorage.tasks = json;
+}
+
+todoList.addEventListener('change', e => {
+  const box = e.target;
+  const li = box.closest('.item');
+  const i = getIndex(li);
+  
+  toggleTaskStatus(i, box.checked);
+  saveTasks();
+})
+
+todoList.addEventListener('click', e => {
+  const a = e.target.closest('.btn-edit');
+  if (!a) return;
+  const li = a.closest('.item');
+  const i = getIndex(li);
+  
+  setTaskToEditIndex(i);
+})
+
+function getIndex(element) {
+  const siblings = Array.from(element.parentElement.children);
+  
+  return siblings.indexOf(element);
+}
+
+function toggleTaskStatus(i, status) {
+  const task = tasks[i];
+  
+  task.done = status;
+}
+
+function setTaskToEditIndex(i) {
+  localStorage.taskToEdit = i;
+}
